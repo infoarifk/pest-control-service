d59af4ed43cc793811a2d50dc858173d97283c22
infoarifk <inbox.arifkhan@gmail.com>
2023-11-14 11:14:23 +0600

    pop up done

--- src/pages/detailspage/Details.jsx
+++ src/pages/detailspage/Details.jsx
@@ -1,17 +1,43 @@
 /* eslint-disable no-unused-vars */
+import { useContext, useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
+import toast, { Toaster } from "react-hot-toast";
+import Popup from "../../components/Popup";
 
 
 const Details = () => {
 
     const service = useLoaderData();
-    const { _id, providerName, providerImg, serviceName, serviceImg, description, price, area } = service;
+    const { user } = useContext(AuthContext);
+    const [isModalOpen, setModalOpen] = useState(false);
+    const { _id, providerName, providerImg, serviceName, serviceImg, description, price, area, providerID } = service;
+    if (!user) {
+
+        return <p className="text-center">Loading...</p>;
+    }
+    
+
+
+    const handleBookNow = () => {
+        if (providerID === user.uid) {
+            toast.error('You can not Book your own services');
+        }
+        setModalOpen(true);
+
+    };
+
+    const handleCloseModal = () => {
+        setModalOpen(false);
+      };
+
+
     return (
         <div className="max-w-[60%] mt-8 mx-auto bg-white rounded-sm">
 
             <img className="rounded-t-sm w-[100%] h-[450px]" src={serviceImg} alt="" />
 
-            <div className="p-5">
+            <div className="p-5 relative">
 
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{serviceName}</h5>
 
@@ -27,9 +53,18 @@ const Details = () => {
                     <p className="font-light text-sm">{area}</p>
                 </div>
 
-                <button className="px-3 py-2 text-sm font-medium text-center text-white bg-[#a02013] rounded-sm hover:bg-rose-600 w-[30%] flex justify-center mx-auto mt-8">
+                
+                <Popup isOpen={isModalOpen} onClose={handleCloseModal}></Popup>
+                <button onClick={handleBookNow} className="px-3 py-2 text-sm font-medium text-center text-white bg-[#a02013] rounded-sm hover:bg-rose-600 w-[30%] flex justify-center mx-auto mt-8">
                     Book Now
                 </button>
+                
+                
+                <Toaster></Toaster>
+
+
+
+
             </div>
         </div>
     );
